f06011ffcb3382854fb8f10c0dd08f9b25f7f60b
0xAYM3N <aymen.dev10@gmail.com>
2025-08-28 16:20:19 +0100

    feat: Footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -5,12 +5,41 @@ import './Footer.css'
 <template>
   <footer class="footer">
     <div class="container">
-      <h2>Ghasla</h2>
-      <div class="links">
-        <router-link to="/">Home</router-link>
-        <a href="#about">About</a>
-        <a href="#service">Service</a>
+      <div class="brand-info">
+        <div class="logo">
+          <img src="/logo.png" alt="Ghasla Logo">
+          <h2>غسلة</h2>
+        </div>
+        <p>
+          خدمة غسيل سيارات احترافية <br />
+          تصلك إلى موقعك
+        </p>
       </div>
+
+      <div class="links">
+        <h3>روابط</h3>
+        <router-link to="/">الرئيسية</router-link>
+        <a href="/#about">من نحن</a>
+        <a href="/#service">الخدمات</a>
+      </div>
+
+      <div class="service-links">
+        <h3>الخدمات</h3>
+        <a href="/#wash">غسيل خارجي</a>
+        <a href="/#interior">تنظيف داخلي</a>
+        <a href="/#premium">باقات مميزة</a>
+      </div>
+
+      <address class="contact">
+        <h3>تواصل معنا</h3>
+        <p><i class="fas fa-phone"></i> (555) 123-4567</p>
+        <p><i class="fas fa-envelope"></i> info@ghasla.com</p>
+      </address>
+    </div>
+
+    <div class="copyright">
+      <p>© {{ new Date().getFullYear() }} غسلة. جميع الحقوق محفوظة.</p>
     </div>
   </footer>
 </template>
+
