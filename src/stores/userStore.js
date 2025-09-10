23d2d47a2bb6e41e652f8ee6ed7340ca3f02569d
0xAYM3N <aymen.dev10@gmail.com>
2025-09-10 15:10:01 +0100

    feat: pinia and save token

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,11 +1,8 @@
-// stores/userStore.js
 import { defineStore } from 'pinia'
-import axios from 'axios'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    token: localStorage.getItem('token') || null,
-    user: null
+    token: localStorage.getItem('token') || null
   }),
 
   getters: {
@@ -17,26 +14,9 @@ export const useUserStore = defineStore('user', {
       this.token = token
       localStorage.setItem('token', token)
     },
-
     logout() {
       this.token = null
-      this.user = null
       localStorage.removeItem('token')
-    },
-
-    async fetchUserData() {
-      if (!this.token) return
-
-      try {
-        const res = await axios.get('http://localhost:3000/users', {
-          headers: { Authorization: `Bearer ${this.token}` }
-        })
-
-        if (!res.data || !res.data.length) throw new Error('No user found')
-        this.user = res.data[0]
-      } catch (err) {
-        console.error('❌ Failed to fetch user:', err)
-      }
     }
   }
 })
