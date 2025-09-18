ba5914d470ab956db98065948e0e0786ecaaef63
0xAYM3N <aymen.dev10@gmail.com>
2025-09-18 07:47:33 +0100

    update the path

--- src/Admin/Statistic/Statistic.vue
+++ src/Admin/Statistic/Statistic.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue"
-import { supabase } from "../../../lib/supabaseClient"
+import { supabase } from "../../lib/supabaseClient"
 import "./Statistic.css"
 
 const usersCount = ref(0)
